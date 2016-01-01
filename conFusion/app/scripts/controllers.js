73ce741f4767385619a36db76b940af338a30914
Andrey Vokhmin <andrey.vokhmin@outlook.com>
2016-01-02 01:12:07 +0300

    + Week#4, Assignment, Task#3;

--- conFusion/app/scripts/controllers.js
+++ conFusion/app/scripts/controllers.js
@@ -57,24 +57,24 @@ angular.module('confusionApp')
                         
         }])
 
-        .controller('FeedbackController', ['$scope', function($scope) {
+        .controller('FeedbackController', ['$scope', 'feedbackFactory', function($scope,feedbackFactory) {
             
             $scope.sendFeedback = function() {
-                
-                console.log($scope.feedback);
                 
                 if ($scope.feedback.agree && ($scope.feedback.mychannel === "")) {
                     $scope.invalidChannelSelection = true;
                     console.log('incorrect');
-                }
-                else {
+                } else {
                     $scope.invalidChannelSelection = false;
-                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-                    $scope.feedback.mychannel="";
-                    $scope.feedbackForm.$setPristine();
                     console.log($scope.feedback);
+                    
+                    feedbackFactory.getFeedback().save($scope.feedback);
+
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedbackForm.$setPristine();
                 }
             };
+
         }])
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
